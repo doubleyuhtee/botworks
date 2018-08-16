db4e99df56cb1a6aacacae60f63119f92d770bac
William Hanson <42045551+doubleyuhtee@users.noreply.github.com>
2018-08-16 18:04:52 -0500

    Break out trigger

--- botworks/chat/InteractionDefinition.py
+++ botworks/chat/InteractionDefinition.py
@@ -1,7 +1,5 @@
-from difflib import SequenceMatcher
-
-import time
 import logging
+import time
 from typing import List
 
 from botworks.config_constants import LOG_LEVEL
@@ -12,19 +10,13 @@ log.setLevel(logging.INFO)
 
 
 class InteractionDefinition:
-    def __init__(self, trigger_definition=None, response=None, as_thread=True,
-                 custom_matcher=None, cooldown_duration_seconds=600,
-                 full_text=None, mod_exempt=True, event=None):
-        self.triggerWords = trigger_definition
+    def __init__(self, trigger, response, cooldown_duration=600, mod_exempt=True):
         self.response = response
-        self.asThread = as_thread
-        self.cooldown_duration_seconds = cooldown_duration_seconds
+        self.trigger = trigger
+        self.cooldown_duration = cooldown_duration
         self.cooldown_end = 0
-        self.fullText = full_text
-        self.matchMethod = custom_matcher
         self.modExempt = mod_exempt
         self.conf = {}
-        self.event = event
 
     def finalize(self, config):
         self.conf = config
@@ -34,84 +26,10 @@ class InteractionDefinition:
         log.info("Checking for response " + str(self.response))
         if self.modExempt and payload.user in mod_ids:
             return False
-        if self.matchMethod:
-            return self.matchMethod(payload)
-        if self.event == payload.event_type:
-            return True
-        if self.fullText and payload.imageText and \
-                SequenceMatcher(None, self.fullText, payload.imageText).ratio() > 0.7:
-            return True
-        if payload.imageText and self.triggerWords and self.triggerWords.check(payload.imageText):
-            return True
-        return self.triggerWords and payload.lower_message and self.triggerWords.check(payload.lower_message)
+        return self.trigger.check(payload)
 
     def respond(self, clacker, payload: Payload):
         if time.time() < self.cooldown_end:
             return
-        self.cooldown_end = time.time() + self.cooldown_duration_seconds
+        self.cooldown_end = time.time() + self.cooldown_duration
         self.response.respond(clacker, payload)
-
-
-class And:
-    def __init__(self, *args):
-        self.triggers = list(map(lambda x: x.lower() if type(x) == str else x, [*args]))
-
-    def check(self, message) -> bool:
-        log.debug("Checking And " + str(len(self.triggers)))
-        res = True
-        for t in self.triggers:
-            if type(t) is str:
-                res &= t in message
-            else:
-                res &= t.check(message)
-                if not res:
-                    break
-        return res
-
-
-class Or:
-    def __init__(self, *args):
-        self.triggers = list(map(lambda x: x.lower() if type(x) == str else x, [*args]))
-
-    def check(self, message) -> bool:
-        log.debug("Checking Or " + str(len(self.triggers)))
-        res = False
-        for t in self.triggers:
-            if type(t) is str:
-                res |= t in message
-            else:
-                res |= t.check(message)
-            if res:
-                break
-        return res
-
-
-class Not:
-    def __init__(self, *args):
-        if len([*args]) != 1:
-            log.error("Misconfigured Not, it's going to get weird")
-        self.triggers = list(map(lambda x: x.lower() if type(x) == str else x, [*args]))
-
-    def check(self, message) -> bool:
-        log.debug("Checking Not " + str(len(self.triggers)))
-        if type(self.triggers[0]) is str:
-            res = self.triggers[0] not in message
-        else:
-            res = not self.triggers[0].check(message)
-
-        return res
-
-
-class StartsWith:
-    def __init__(self, *args):
-        if len([*args]) != 1:
-            log.error("Misconfigured Startswith, it's going to get weird")
-        self.triggers = list(map(lambda x: x.lower() if type(x) == str else x, [*args]))
-
-    def check(self, message: str) -> bool:
-        log.debug("Checking StartsWith " + str(len(self.triggers)))
-        if type(self.triggers[0]) is str:
-            return message.startswith(self.triggers[0])
-
-        return False
-
