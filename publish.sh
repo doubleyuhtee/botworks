5f330413060bc5510673751a603a0f27355bbfe6
William Hanson <42045551+doubleyuhtee@users.noreply.github.com>
2018-08-28 08:24:31 -0500

    Release 0.1.1

--- publish.sh
+++ publish.sh
@@ -7,7 +7,7 @@ git tag $ver
 
 rm dist/*
 python3 setup.py sdist bdist_wheel
-twine upload dist/* -r pypi
+#twine upload dist/* -r pypi
 
 cat version.txt
 
@@ -15,5 +15,5 @@ nextVersion=$(awk -F '[.-]' 'NR==1{ print $1"."$2"."($3 + 1) }' version.txt)
 echo "${nextVersion}-SNAPSHOT" > version.txt
 git commit -am "Next version"
 
-git push
-git push --tags
+#git push
+#git push --tags
