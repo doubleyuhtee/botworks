9c8f74c789647db0874f837d4b48e4dab5db93bd
William Hanson <42045551+doubleyuhtee@users.noreply.github.com>
2018-08-28 08:20:33 -0500

    build/publish stuff

--- publish.sh
+++ publish.sh
@@ -1,3 +1,17 @@
+ver=$(awk -F '[.-]' 'NR==1{ print $1"."$2"."$3 }' version.txt)
+echo $ver
+
+echo $ver > version.txt
+git commit -am "Release $ver"
+git tag $ver
+
 rm dist/*
 python3 setup.py sdist bdist_wheel
 twine upload dist/* -r pypi
+
+nextVersion=$(awk -F '[.-]' 'NR==1{ print $1"."$2"."($3 + 1) }' version.txt)
+echo "${nextVersion}-SNAPSHOT" > version.txt
+git commit -am "Next version"
+
+git push
+git push --tags
