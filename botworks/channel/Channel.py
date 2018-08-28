f97cdafbace8de85eefd6b652eadd4b27b5596ff
William Hanson <42045551+doubleyuhtee@users.noreply.github.com>
2018-08-28 08:20:24 -0500

    Response sends all configured and new class Responses picks from a list of Response

--- botworks/channel/Channel.py
+++ botworks/channel/Channel.py
@@ -1,6 +1,8 @@
+from botworks.chat.Response import IResponse
+
 
 class Channel:
-    def __init__(self, name, mod_names, responses):
+    def __init__(self, name, mod_names, responses: IResponse):
         self.channelName = name
         self.mod_names = mod_names
         self.responses = responses
--- botworks/chat/Response.py
+++ botworks/chat/Response.py
@@ -1,29 +1,49 @@
 import random
 
 
-class Response:
+class IResponse:
+    def respond(self, client, payload): pass
+
+
+class Response(IResponse):
     def __init__(self, text=None, emoji=None, ghostly=None, threaded=None, method=None):
-        self.text = text
-        self.emojis = emoji
-        self.ghostly = ghostly
-        self.threaded_response = threaded
+        self.text = self.__to_list(text)
+        self.emojis = self.__to_list(emoji)
+        self.ghostly = self.__to_list(ghostly)
+        self.threaded_response = self.__to_list(threaded)
         self.method = method
 
     def respond(self, client, payload):
         if self.method:
             self.method(client=client, payload=payload)
         if self.text:
-            client.post_message(channel=payload.channel, message=self.get_response_value(self.text))
+            for t in self.text:
+                client.post_message(channel=payload.channel, message=t)
         if self.emojis:
-            client.react(payload, self.get_response_value(self.emojis))
+            for e in self.emojis:
+                client.react(payload, e)
         if self.ghostly:
-            client.ephemeral(payload, self.get_response_value(self.ghostly))
+            for e in self.ghostly:
+                client.ephemeral(payload, e)
         if self.threaded_response:
-            client.thread_reply(payload, self.get_response_value(self.threaded_response))
+            for m in self.threaded_response:
+                client.thread_reply(payload, m)
 
     @staticmethod
-    def get_response_value(options):
-        res = options
-        if type(options) is list:
-            res = random.choice(options)
-        return res
+    def __to_list(o):
+        if type(o) is list:
+            return o
+        return [o]
+
+
+class Responses(IResponse):
+    def __init__(self, responses):
+        self.responses = responses
+
+    def respond(self, client, payload):
+        if type(self.responses) is list:
+            random.choice(self.responses).respond(client, payload)
+        else:
+            self.responses.respond(client, payload)
+
+
